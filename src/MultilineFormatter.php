67d8ce734049637c290b7c7ed2e5069ff7f0adbd
Anton Tyutin <anton@tyutin.ru>
2019-09-12 16:39:37 +0300

    Verbose exceptions formatting

--- src/MultilineFormatter.php
+++ src/MultilineFormatter.php
@@ -3,14 +3,15 @@
 namespace Nalogka\Monolog\Formatter;
 
 use Monolog\Formatter\LineFormatter as BaseLineFormatter;
+use Monolog\Utils;
 
 /**
  * Форматирует запись с выводом контекста и дополнительной информации
  * с отступом для удобного разбора.
- * 
+ *
  * Первая строка записи начинается с метки времени, заключенной в
  * квадратые скобки. Многострочный текст сообщения распечатывается с отступом в
- * удобочтаемом виде. После сообщения так же с отступом распечатывается 
+ * удобочтаемом виде. После сообщения так же с отступом распечатывается
  * структура контекста и дополнительные данные записи.
  */
 class MultilineFormatter extends BaseLineFormatter
@@ -41,6 +42,73 @@ class MultilineFormatter extends BaseLineFormatter
         $extraStr = $extra ? "\n" . $this->stringifyIndented($this->normalize($extra), self::INDENT_STRING) : '';
 
         return $this->stringifyIndented($str, self::INDENT_STRING, '') . $extraStr . "\n";
+    }
+
+    protected function normalizeException($e)
+    {
+        if (!$e instanceof \Exception && !$e instanceof \Throwable) {
+            throw new \InvalidArgumentException(
+                'Exception/Throwable expected, got ' . gettype($e) . ' / ' . Utils::getClass($e)
+            );
+        }
+
+        $data = array(
+            'class' => Utils::getClass($e),
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile() . ':' . $e->getLine(),
+        );
+
+        if ($this->includeStacktraces) {
+            $trace = $e->getTrace();
+            $traceLen = count($trace);
+            foreach ($trace as $idx => $frame) {
+                $idx = sprintf('#%02u', $traceLen - $idx);
+                if (isset($frame['file'])) {
+                    $args = isset($frame['args']) ? $this->stringifyCallArgs($frame['args']) : '';
+                    $call = (isset($frame['class']) ? $frame['class'] . $frame['type'] : '') . $frame['function'] . '(' . $args . ')';
+                    $data['trace'][$idx] = $call . ' в ' . $frame['file'] . ':' . $frame['line'];
+                } elseif (isset($frame['function']) && $frame['function'] === '{closure}') {
+                    $data['trace'][$idx] = $frame['function'];
+                } else {
+                    // We should again normalize the frames, because it might contain invalid items
+                    $data['trace'][$idx] = $this->normalize($frame);
+                }
+            }
+        }
+
+        if ($previous = $e->getPrevious()) {
+            $data['previous'] = $this->normalizeException($previous);
+        }
+
+        return $data;
+    }
+
+    private function stringifyCallArgs(array $args)
+    {
+        return implode(', ', array_map([$this, 'stringifyCallArg'], $args ?? []));
+    }
+
+    private function stringifyCallArg($argValue)
+    {
+        switch (gettype($argValue)) {
+            case 'NULL':
+            case 'boolean':
+            case 'integer':
+            case 'double':
+            case 'string':
+            case 'array':
+                return json_encode($argValue);
+            case 'object':
+                $json_encode = json_encode($argValue);
+
+                return get_class($argValue) . '#' . spl_object_hash($argValue) . ' ' . $json_encode;
+            case 'resource':
+            case 'resource (closed)':
+                return (string)$argValue . '(' . get_resource_type($argValue) . ')';
+            default:
+                return '?' . gettype($argValue) . '?';
+        }
     }
 
     private function stringifyIndented($data, $indentation = '', $leadingIndentation = null)
